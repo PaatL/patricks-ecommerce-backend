14af75affa11d2a326804acc67c16d3e0b3d75bd
Patrick <lipatrick093@gmail.com>
2024-02-14 15:30:29 -0500

    final product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,6 +21,17 @@ router.get("/", async(req, res) => {
 
 // get one product
 router.get("/:id",async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id,
+      {
+        include:[Category,{model: Tag, through: ProductTag}]
+      }
+    );
+    res.status(200).json(productData);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -101,8 +112,21 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+            where: {
+        id: req.params.id
+      }
+      }) ;
+  if(!productData){
+    res.status(404).json({ message: "Product doesn't exist"});
+    return;
+  }
+  res.status(200).json(productData);
   // delete one product by its `id` value
+} catch(err) {
+  res.status(500).json(err);
+}
 });
-
 module.exports = router;
